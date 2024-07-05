8c42762280a34dbf2f5d61f88b4207687b304039
alex_ <alex_7377@hotmail.com>
2024-07-05 12:20:23 +0200

    fourth commit

--- src/TaskFirst.java
+++ src/TaskFirst.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class TaskFirst {
+
     public static void main(String[] args) {
+
         Car wolga = new Car("Wolga", 1995, 550_000);
         Car vw = new Car("VW", 2005, 120_000);
         Car volvo = new Car("Volvo", 2008, 90_000);
@@ -12,6 +14,7 @@ public class TaskFirst {
         Car opel = new Car("Opel", 2020, 89_000);
         Car honda = new Car("Honda", 2100, 160_000);
         Car ford = new Car();
+
         List<Car> cars = new ArrayList<>();
         cars.add(wolga);
         cars.add(vw);
@@ -24,21 +27,22 @@ public class TaskFirst {
         cars.add(honda);
         cars.add(ford);
 
-        System.out.println("Список машин : " + cars);
+        System.out.println("\nСписок машин :");
+        cars.forEach(System.out::println);
 
         int totalMileage = cars.stream()
                 .filter(car -> car.getName() != null && car.getManufactureYear() > 1999)
                 .mapToInt(Car::getMileage)
                 .sum();
 
-        System.out.println("Общий пробег автомобилей новее 1999 года: " + totalMileage + " км");
+        System.out.println("\nОбщий пробег автомобилей новее 1999 года:\n" + totalMileage + " км");
 
         Optional<Car> oldCar = cars.stream()
                 .filter(car -> car.getName() != null && car.getMileage() > 500_000)
                 .min(Comparator.comparing(Car::getManufactureYear));
         String nameOfOldCar = String.valueOf(oldCar.orElse(null));
 
-        System.out.println("Самая старая машина с пробегом более 500 000 км: " + nameOfOldCar);
+        System.out.println("\nСамая старая машина с пробегом более 500 000 км:\n" + nameOfOldCar);
 
         Optional<Car> carWithTheHighestMileage = cars.stream()
                 .filter(car -> car.getName() != null && car.getMileage() <= 200_000 &&
@@ -46,8 +50,8 @@ public class TaskFirst {
                 .max(Comparator.comparing(Car::getMileage));
         String nameOfCarWithTheHighestMileage = String.valueOf(carWithTheHighestMileage.orElse(null));
 
-        System.out.println("Машина с маркой на 'V' или 'W' " +
-                "и пробегом не более 200 000 км с наибольшим пробегом: "
+        System.out.println("\nМашина с маркой на 'V' или 'W' " +
+                "и пробегом не более 200 000 км с наибольшим пробегом:\n"
                 + nameOfCarWithTheHighestMileage);
     }
 }
